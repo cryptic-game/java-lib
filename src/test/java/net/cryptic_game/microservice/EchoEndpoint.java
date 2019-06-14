5e40a19931bc07dba16305f3a87b8606cb64199a
use-to <use-to@outlook.de>
2019-06-15 00:30:56 +0200

    implemented contact microservice functionality

--- src/test/java/net/cryptic_game/microservice/EchoEndpoint.java
+++ src/test/java/net/cryptic_game/microservice/EchoEndpoint.java
@@ -1,5 +1,7 @@
 package net.cryptic_game.microservice;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import org.json.simple.JSONObject;
@@ -8,11 +10,18 @@ import net.cryptic_game.microservice.endpoint.UserEndpoint;
 
 public class EchoEndpoint {
 
-	@UserEndpoint(path = { "echo" },keys = { "message" }, types = { String.class })
+	@UserEndpoint(path = { "echo" }, keys = { "message" }, types = { String.class })
 	public static JSONObject echo(JSONObject data, UUID user) {
 		Echo.create((String) data.get("message"));
-		
+
+		Map<String, Object> jsonMap = new HashMap<String, Object>();
+
+		jsonMap.put("device_uuid", UUID.randomUUID().toString());
+
+		System.out.println(
+				MicroService.instance.contactMicroservice("device", new String[] { "exist" }, new JSONObject(jsonMap)));
+
 		return data;
 	}
-	
+
 }
