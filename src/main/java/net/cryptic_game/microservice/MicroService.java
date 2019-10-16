e95f26b2de728184cc21ceb9b62b5a467762f100
Marcel Koch <koch_marcel@outlook.com>
2019-10-16 22:33:39 +0200

    Added Sleep between reconnect & Updated Reflections. (#22)

--- src/main/java/net/cryptic_game/microservice/MicroService.java
+++ src/main/java/net/cryptic_game/microservice/MicroService.java
@@ -60,7 +60,7 @@ public abstract class MicroService extends SimpleChannelInboundHandler<String> {
 
         instance = this;
 
-        if(!Config.get(DefaultConfig.SENTRY_DSN).equals("")) {
+        if (!Config.get(DefaultConfig.SENTRY_DSN).equals("")) {
             Sentry.init(Config.get(DefaultConfig.SENTRY_DSN));
         }
 
@@ -121,6 +121,12 @@ public abstract class MicroService extends SimpleChannelInboundHandler<String> {
 
             Sentry.capture(e);
 
+            try {
+                Thread.sleep(10000L);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+
             // reconnect
             start();
         }
@@ -140,7 +146,7 @@ public abstract class MicroService extends SimpleChannelInboundHandler<String> {
 
             JSON json = new JSON(jsonObject);
 
-            if(json.get("error") != null) {
+            if (json.get("error") != null) {
                 return;
             }
 
